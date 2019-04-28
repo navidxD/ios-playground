7044d326599d95353d27e99fce380d808dcd9b89
David Ordaz <navid.0106@gmail.com>
2019-04-28 17:56:00 -0500

    primera tarea coursera

--- Coursera1.playground/Contents.swift
+++ Coursera1.playground/Contents.swift
@@ -1,3 +1,39 @@
 import UIKit
 
-var str = "Hello, playground"
+var str = "Hello, Coursera1"
+
+/*
+ - Si el número es divisible entre 5, imprime: # el número  + “Bingo!!!”
+ 
+ - Si el número es par, imprime: # el número + “par!!!”
+ 
+ - Si el número es impar, imprime: # el número + “impar!!!”
+ 
+ - Si el número se encuentra dentro de un rango del 30 al 40, imprime: # el número +  “Viva Swift!!!”
+ */
+
+let RANGE:ClosedRange<Int> = 0...100
+
+var num:Int;
+
+for num in RANGE {
+    if(num%5 == 0) {
+        print("# \(num)"  + "Bingo!!!")
+    }
+    
+if(num%2 == 0) {
+         print("# \(num)"  + "Par!!!")
+    }
+    
+    if(num%2 != 0) {
+        print("# \(num)"  + "Impar!!!")
+    }
+    
+    switch num {
+    case 30...40:
+        print("# \(num)"  + "Viva Swift!!!")
+    default:
+        print("# \(num)")
+    }
+}
+
